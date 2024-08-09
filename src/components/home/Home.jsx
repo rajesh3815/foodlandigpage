097f0b483a6130d60c5f13d0487a8aa503efb6b4
Rajesh Kumar <rkraja730@gmail.com>
2024-08-09 20:42:18 +0530

    home added

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Style from "./Home.module.css";
+import logotruck from "../../assets/logotruck.png";
+import pizzaImg from "../../assets/pizzaImg.png";
+import sideVector from "../../assets/sidevector.png";
+const Home = () => {
+  return (
+    <div className={Style.mainContainer}>
+      <div className={Style.containerLeft}>
+        <img src={logotruck} alt="logo" loading="eager" />
+        <div className="leftContainerFooter">
+          <h1>
+            Discover the <span>Best</span> Food and Drinks
+          </h1>
+          <p>
+            Naturally made Healthcare Products for the better care & support of
+            your body.
+          </p>
+          <button>Explore Now!</button>
+        </div>
+      </div>
+      <div className={Style.containerRight}>
+        <div className={Style.innerRight}>
+          <img className={Style.pizzaImg} src={pizzaImg} alt="pizza" />
+          <img className={Style.vectorImg} src={sideVector} alt="side" />
+        </div>
+        <button>Get In Touch</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
